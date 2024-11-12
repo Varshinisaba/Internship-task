9e8a2902175bcb483a171d4f2c6966e1bf164451
Varshinisaba <162007348+Varshinisaba@users.noreply.github.com>
2024-11-12 23:42:57 +0530

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,1 +1,26 @@
-..
+// app/components/Footer.tsx
+export default function Footer() {
+    return (
+      <footer className="footer">
+        <div className="footer-content">
+          <div>
+            <p>Logo</p>
+            <p>SentinelGuardAI</p>
+          </div>
+          <div>
+            <p>For information on our services:</p>
+            <p>Contact us for more information.</p>
+          </div>
+          <div>
+            <p>Company</p>
+            <p>About us</p>
+          </div>
+          <div>
+            <p>Get in touch</p>
+            <p>contact@sentinelguardai.com</p>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+  
